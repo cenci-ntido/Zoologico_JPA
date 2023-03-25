93c0ab35e278a899459edf0fea1727f1ea12fe93
55469 <55469@cenci>
2023-03-25 17:20:20 -0300

    Merge origin/master
    Conflicts:
    	src/main/java/utfpr/aulajpa/util/Menu.java

--- src/main/java/utfpr/aulajpa/App.java
+++ src/main/java/utfpr/aulajpa/App.java
@@ -1,68 +1,58 @@
-package utfpr.aulajpa;
 
-import java.util.List;
 import java.util.Scanner;
 import javax.persistence.EntityManager;
 import utfpr.aulajpa.dao.EspecieDAO;
 import utfpr.aulajpa.model.Especie;
 import utfpr.aulajpa.util.Factory;
-import utfpr.aulajpa.util.Menu;
 
 public class App {
 
+    private static Scanner scanner = new Scanner(System.in);
+    private static EntityManager em = Factory.getEntityManager();
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        EntityManager em = Factory.getEntityManager();
+        boolean sair = false;
         em.getTransaction().begin();
-        Menu.printGeral();
-        int opcao = scanner.nextInt();
-        switch (opcao) {
-            case 0:
-                break;
-            case 1:
-                Menu.printEspecies();
-                int opcaoD = scanner.nextInt();
-                EspecieDAO especieDao = new EspecieDAO(em);
-                Especie especie = new Especie();
-                switch (opcaoD) {
-                    case 0:
-                        break;
-                    case 1:
-                        System.out.println("Informe o nome da espécie:\n");
-                        scanner.nextLine();
-                        String nomeEsp = scanner.nextLine();
-                        scanner.nextLine();                 
-                        especie = new Especie(nomeEsp);
-                        especieDao.salvar(especie);
-                        System.out.println("Espécie Cadastrada!");
-                        break;
-                    case 2:
-                        System.out.println("Informe o código da espécie para atualizar o nome:\n");
-                        long cod = scanner.nextLong();
-                        scanner.nextLine();
-                        especie = especieDao.buscaEspecie(cod);
-                        System.out.println("Informe o novo nome da espécie:\n");
-                        String novoNome = scanner.nextLine();
-                        scanner.nextLine();
-                        especie.setNome(novoNome);
-                        especieDao.atualizar(especie);
-                        System.out.println("Nome de espécie atualizado!");
-                        break;
-                    case 3:
-                        System.out.println("Informe o código da espécie para excluir:\n");
-                        long codEsp = scanner.nextLong();
-                        scanner.nextLine();
-                        especie = especieDao.buscaEspecie(codEsp);
-                        especieDao.excluir(especie);
-                        System.out.println("Espécie excluida!");
-                        break;
-                }
-                break;
-        }
-        System.out.println("\nAté mais!\n");
-        em.getTransaction().commit();
-        em.close();
-    }
 
+        while (!sair) {
+            System.out.println("================MENU================");
+            System.out.println(
+                    "0 - Sair\n"
+                    + "1 - Cadastrar Espécie\n"
+                    + "2 - Cadastrar Profissional\n"
+                    + "3 - Cadastrar Animal\n"
+                    + "4 - Cadastrar serviço\n"
+                    + "5 - Listar Serviços não realizados\n");
+            System.out.println("==========Informe uma Opção=========\n");
+            int opcao = scanner.nextInt();
+            switch (opcao) {
+                case 0:
+                    sair = true;
+                    break;
+                case 1:
+                    System.out.println("Informe o nome da espécie:\n");
+                    scanner.nextLine();
+                    String nome = scanner.nextLine();
+                    scanner.nextLine();
+                    EspecieDAO especieDAO = new EspecieDAO(em);
+                    Especie especie = new Especie(nome);
+                    especieDAO.salvar(especie);
+                    System.out.println("Espécie Cadastrada!");                   
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+                    break;
+            }
+        }
+
+        System.out.println("Até mais!");
+        em.close();
+        scanner.close();
+
+    }
 }
